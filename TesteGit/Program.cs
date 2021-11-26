348d1802887e2247e01b464f87793f744799ffd9
João <johhdq@hotmail.com>
2021-11-26 10:32:40 -0300

    Foi adicionada uma calculadora do 1 ao 10

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -10,7 +10,22 @@ namespace TesteGit
             Console.WriteLine("Bem-vindo ao projeto de teste do Git!!");
             Console.WriteLine("Bom dia!!!");
 
-            Console.ReadLine();
+            Console.WriteLine("TABUADA!");
+            Console.Write("\nTabuada do? ");
+            int tabuada = int.Parse(Console.ReadLine());
+            
+            if (tabuada >= 1 && tabuada <= 10)
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    Console.WriteLine($"{tabuada} x {i} = {tabuada * i}");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Error: Tabuada inválida!");
+            }
+
 
 
         }
