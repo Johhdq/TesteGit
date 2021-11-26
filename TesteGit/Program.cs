f49f47fed870c559b656a4c1854bf03d81482669
João <johhdq@hotmail.com>
2021-11-26 10:16:18 -0300

    Projeto iniciado com mensagem Olá mundo e boas vindas

---
+++ TesteGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem-vindo ao projeto de teste do Git!!");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
